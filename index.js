0a5fa500971cf230fea8514610ce11a0ed550d64
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-10 15:25:50 +0530

    Added .env and accessed it from index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
 
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
 app.use(express.static('static'));
+
+console.log(config);
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
@@ -13,3 +22,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+module.exports = config;
